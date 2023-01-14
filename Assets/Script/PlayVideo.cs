b17ea087d773d9bdbd978fdb5abd1ec9f466d7e0
Logan Smith <logan@argyle.build>
2023-01-14 17:29:35 -0500

    Video trigger

---
+++ Assets/Script/PlayVideo.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    public GameObject originalObject;
+    public GameObject videoScreen;
+
+    void Start()
+    {
+        originalObject.SetActive(true);
+        videoScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider Other)
+    {
+        if (Other.tag == "Player")
+        {
+            videoScreen.SetActive(true);
+            originalObject.SetActive(false);
+        }
+       
+      
+    }
+
+    
+    }
